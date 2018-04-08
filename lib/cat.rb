efbd5a8f6eb6af40f89cfc0c97b0c0b54da9f70f
Shun Yao <ysmiracle@hotmail.com>
2018-04-08 19:17:41 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   attr_reader :name
   attr_accessor :mood
 
-  def initialize(name, mood)
+  def initialize(name)
     @name = name
     @mood = "nervous"
   end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_reader :name
   attr_accessor :mood
-  
+
   def initialize(name)
     @name = name
     @mood = "nervous"
